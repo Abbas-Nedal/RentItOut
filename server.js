86fa449ab05e845703c9eb6e073b4404f2e8a6fb
shehab-kharaz <shehababbaskharaz.222@hotmail.com>
2024-10-23 19:00:12 +0300

    Adding Winstone logger and Update README
    - Implemented request logging using Winston.
    - Created a 'logs-output' folder to store log files and configured logging in a new 'config/logger.js' file.
    - Integrated the logger into 'server.js' to log incoming requests with HTTP method, URL, version, and user-agent.
    - Updated the project structure in the README to reflect the new 'config' folder and logging setup.
    - Updated the Technologies section in the README to include Winston for logging and other technologies.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./database');
+const logger = require('./logger'); 
 
 const userRoutes = require('./routes/userRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
@@ -8,6 +9,13 @@ const logisticRoutes = require('./routes/logisticRoutes');
 
 const app = express();
 app.use(bodyParser.json());
+
+// Middleware to log requests
+app.use((req, res, next) => {
+    const logMessage = `${req.method} ${req.url} - HTTP/${req.httpVersion} - ${req.headers['user-agent']}`;
+    logger.info(logMessage);
+    next();
+});
 
 // Test DB connection
 db.getConnection()
@@ -24,5 +32,5 @@ app.use('/logistics', logisticRoutes);
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    logger.info(`Server running on port ${PORT}`);
 });
