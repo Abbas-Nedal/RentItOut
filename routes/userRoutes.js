ca3732829723970585c8e6ef371a569fd66eadc0
shehab-laptop <shehababbaskharaz.222@hotmail.com>
2024-10-27 00:15:24 +0200

    Implementng user authentication and authorization
    - NOT finished yet. In progress
    - login functionality, JWT token generation, authentication middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,30 @@
-// routes/userRoutes.js
 const express = require('express');
 const userController = require('../controllers/userController');
+const roleMiddleware = require('../middlewares/roleMiddleware');
+const userValidation = require('../validations/userValidation'); 
+const authMiddleware = require('../middlewares/authMiddleware'); 
 
 const router = express.Router();
 
+router.use(authMiddleware.authenticateToken); 
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post('/login', userController.loginUser); 
+
+router.get('/', userController.getUsers); 
+router.get('/:id', userController.getUserById); 
+
+router.post('/', 
+  userValidation.createUserValidation, 
+  roleMiddleware.requireAdminRole, 
+  userController.createUser); 
+
+router.put('/:id', 
+  userValidation.updateUserValidation, 
+  roleMiddleware.requireAdminRole, 
+  userController.updateUser); 
+
+router.delete('/:id', roleMiddleware.requireAdminRole, 
+  userController.deleteUser); 
+
 
 module.exports = router;
---
+++ validations/userValidation.js
@@ -0,0 +1,25 @@
+const { body } = require('express-validator');
+
+exports.createUserValidation = [
+    body('name').isString().trim().notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password')
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[@$!%*?&#]/).withMessage('Password must contain at least one special character'),
+    body('phone_number').optional().isMobilePhone().withMessage('Invalid phone number'),
+    body('address').optional().isString().trim(),
+    body('role').isIn(['user', 'admin']).withMessage('Invalid role'),
+    body('verified').isBoolean().withMessage('Verified must be true or false'),
+];
+
+exports.updateUserValidation = [
+    body('email').optional().isEmail().withMessage('Invalid email address'),
+    body('password')
+        .optional()
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters')
+        .matches(/[A-Z]/).withMessage('Password must contain at least one uppercase letter')
+        .matches(/[0-9]/).withMessage('Password must contain at least one number')
+        .matches(/[@$!%*?&#]/).withMessage('Password must contain at least one special character'),
+];
