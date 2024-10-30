f0e8a6cb715ce0e231970e3a7f408dcaf14d2988
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2024-10-30 21:24:12 +0200

    Implement processPayment functilanty

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -23,7 +23,7 @@ exports.initializePayment = async (req, res) => {
         }
         const [rental] = await db.query(`SELECT * FROM rentals WHERE id = ? AND status = 'pending'`, [rentalId]);
         if (!rental) {
-            return res.status(404).json({ error: "Rental not found or already completed." });
+            return res.status(404).json({ error: "Rental not found or completed" });
         }
     //process
         const result = await db.query(
@@ -37,7 +37,40 @@ exports.initializePayment = async (req, res) => {
     }
 };
 exports.processPayment = async (req, res) => {
-
+    try {
+        const { rentalId, paymentId } = req.params;
+        //validate
+        if (!rentalId || !paymentId ) {
+            return res.status(400).json({ error: "All required fields must be provided" });
+        }
+        const [payment] = await db.query(
+            `SELECT * FROM payment_transactions WHERE id = ? AND rental_id = ? AND status = 'pending'`,
+            [paymentId, rentalId]
+        );
+        if (!payment) {
+            return res.status(404).json({ error: "Payment not found or processed" });
+        }
+        //process
+        const paymentSuccess = true;
+        // firstly, it may ambigous, but acttuly i supposed if there a real pay so here we should call real paymnet
+        // like PayPal, Visa ....., so it may be failed if no money
+        if (paymentSuccess) {
+            await db.query(
+                `UPDATE payment_transactions SET status = 'paid' WHERE id = ?`,
+                [paymentId]
+            );
+            await db.query(
+                `UPDATE rentals SET status = 'completed' WHERE id = ?`,
+                [rentalId]
+            );
+            res.json({ message: "Payment processed successfully" });
+        } else {
+            res.status(400).json({ error: "Payment failed" });
+        }
+    } catch (error) {
+        if (debug)  console.error("Error  in paymentController/processPayment:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
 
