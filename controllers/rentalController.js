1c915979c6f487aba8768a987ff2863a2332ff07
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2024-10-31 07:24:18 +0200

    implement createRental to init rental process

--- controllers/rentalController.js
+++ controllers/rentalController.js
@@ -18,10 +18,48 @@
 const db = require('../database');
 
 const debug = true;
+const platform_fee_percentage  = 0.1
+const insurance_fee_percentage  = 0.05
 
 exports.createRental = async (req, res) => {
+    try {
+        const {
+            user_id,
+            item_id,
+            quantity,
+            start_date,
+            end_date
+        } = req.body;
 
+    //check
+        if (!user_id || !item_id || !quantity || !start_date || !end_date) {
+            return res.status(400).json({ error: "All required fields must be provided" });
+        }
+    //process
+        const itemPricePerDay = await db.query(
+            `SELECT price_per_day FROM items WHERE id = ?`, [item_id]
+        );
+        if (itemPricePerDay.length === 0) {
+            return res.status(404).json({ error: "Item not found" });
+        }
+        const dailyPrice = itemPricePerDay[0].price_per_day;
+        const rentalDays = (new Date(end_date) - new Date(start_date)) / (1000 * 60 * 60 * 24);
+        const totalItemPrice = dailyPrice * quantity * rentalDays;
+        const insurance_fee= insurance_fee_percentage*totalItemPrice
+        const  platform_fee= platform_fee_percentage*totalItemPrice
+
+        const total_price = totalItemPrice + (insurance_fee || 0) + (platform_fee || 0);
+        const newRental = await db.query(
+            `INSERT INTO rentals (user_id, item_id,created_at, quantity, start_date, end_date, insurance_fee, platform_fee, status, total_price) VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, 'pending', ?)`,
+            [user_id, item_id, quantity, start_date, end_date, insurance_fee, platform_fee, total_price]
+        );
+        res.status(201).json({ message: "Rental created", rental: { id: newRental.insertId, user_id, item_id, quantity, start_date, end_date, insurance_fee, platform_fee, total_price, status: 'pending'} });
+    } catch (error) {
+        if (debug )console.error("Error rentalController/creatRental:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
+
 exports.cancelRental = async (req, res) => {
 
 };
